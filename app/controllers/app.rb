2acbdb8b0bbe0276abee4837d923a6a2b9ad5d46
hank liu <hankliucode@gmail.com>
2024-04-19 01:24:21 +0800

    feat: CRUD for postit

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -1,17 +1,58 @@
 # frozen_string_literal: true
 
 require 'roda'
-require_relative '../models/location'
+require 'json'
+
+require_relative '../models/postit'
 
 module ChitChat
   # Main App
   class Api < Roda
-    plugin :json
+    plugin :environments
     plugin :halt
 
-    route do |r|
-      r.root do
-        { message: 'It works!' }
+    configure do
+      Postit.setup
+    end
+
+    route do |routing| # rubocop:disable Metrics/BlockLength
+      routing.root do
+        response.status = 200
+        { message: 'ChitChatAPI up at /api/v1' }.to_json
+      end
+
+      routing.on 'api' do
+        routing.on 'v1' do
+          routing.on 'postits' do
+            # GET api/v1/postits/[id]
+            routing.get String do |id|
+              response.status = 200
+              Postit.find(id).to_json
+            rescue StandardError
+              routing.halt 404, { message: 'Postit not found' }.to_json
+            end
+
+            # GET api/v1/postits
+            routing.get do
+              response.status = 200
+              output = { postit_ids: Postit.all }
+              JSON.pretty_generate(output)
+            end
+
+            # POST api/v1/postits
+            routing.post do
+              new_data = JSON.parse(routing.body.read)
+              new_postit = Postit.new(new_data)
+
+              if new_postit.save
+                response.status = 201
+                { message: 'Postit created', id: new_postit.id }.to_json
+              else
+                routing.halt 400, { message: 'Could not create postit' }.to_json
+              end
+            end
+          end
+        end
       end
     end
   end
