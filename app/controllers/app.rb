1f24b8d98401f6ad4a507f3e83389fab1d0fd226
Tony Yang <t510599@gmail.com>
2024-04-23 00:58:14 +0800

    feat(Event): app routes

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -49,6 +49,39 @@ module ChitChat
             routing.halt 500, { message: 'Database error' }
           end
         end
+
+        routing.on 'events' do
+          # GET api/v1/events/[id]
+          routing.get String do |event_id|
+            response.status = 200
+            event = Event.first(id: event_id)
+            event ? event.to_json : raise('Event not found')
+          rescue StandardError => e
+            routing.halt 404, { message: e.message }
+          end
+
+          # GET api/v1/events
+          routing.get do
+            response.status = 200
+            { event_ids: Event.all }
+          end
+
+          # POST api/v1/events
+          routing.post do
+            new_data = JSON.parse(routing.body.read)
+            new_event = Event.create(new_data)
+
+            if new_event
+              response.status = 201
+              { message: 'Event created', id: new_event.id }
+            else
+              routing.halt 400, { message: 'Could not create event' }
+            end
+          rescue StandardError => e
+            puts e.message
+            routing.halt 500, { message: 'Database error' }
+          end
+        end
       end
     end
   end
