ced4d9a0717b66c38438d534cab92cf849b8d6a9
hank liu <hankliucode@gmail.com>
2024-05-12 16:47:59 +0800

    fix: support https

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -10,15 +10,26 @@ module ChitChat
     plugin :json
     plugin :multi_route
 
+    def secure_request?(routing)
+      routing.scheme.casecmp(Api.config.SECURE_SCHEME).zero?
+    end
+
     route do |routing|
+      response['Content-Type'] = 'application/json'
+
+      secure_request?(routing) ||
+        routing.halt(403, { message: 'TLS/SSL Required' }.to_json)
+
       routing.root do
         response.status = 200
         { message: 'ChitChatAPI up at /api/v1' }
       end
 
-      @api_root = 'api/v1'
-      routing.on @api_root do
-        routing.multi_route
+      routing.on 'api' do
+        routing.on 'v1' do
+          @api_root = 'api/v1'
+          routing.multi_route
+        end
       end
     end
   end
--- spec/integration/api_spec.rb
+++ spec/integration/api_spec.rb
@@ -8,4 +8,10 @@ describe 'Test ChitChat Web API' do
     _(last_response.status).must_equal 200
     _(last_response.body).must_include 'ChitChatAPI up at /api/v1'
   end
+
+  it 'should not work if https is used, http is used in dev and test' do
+    get '/path', {}, 'rack.url_scheme' => 'https'
+    _(last_response.status).must_equal 403
+    _(last_response.body).must_include 'TLS/SSL Required'
+  end
 end
